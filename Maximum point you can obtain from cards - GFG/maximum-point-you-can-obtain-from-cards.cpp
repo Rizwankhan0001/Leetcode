3ca054d64c1b522b864076a2dd26fcec4f8f04b5
Rizwankhan0001 <113703223+Rizwankhan0001@users.noreply.github.com>
2023-07-23 20:46:41 +0530

    Added solution - LeetHub

---
+++ Maximum point you can obtain from cards - GFG/maximum-point-you-can-obtain-from-cards.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long maxScore(vector<int> &cardPoints, int N, int k) {
+        // code here
+        
+        vector<int>sum(N,0);
+        
+        int Sum=0;
+        for(int i=0;i<N;i++){
+             Sum+=cardPoints[i];
+            //sum[i] will be equal to sum at each step
+            sum[i]=Sum;
+        }
+        if(N==k) return Sum;
+        int score=0;
+        int ans=0;
+        for(int i=0;i<=k;i++)
+        {
+            int j=i+N-k-1;
+            if(i==0) ans=sum[j];
+            else ans=sum[j]-sum[i-1];
+            
+            score=max(score,Sum-ans);
+            
+            
+        }
+        return score;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, k;
+        cin >> N >> k;
+
+        vector<int> cardPoints(N);
+        for (int i = 0; i < N; i++) cin >> cardPoints[i];
+
+        Solution obj;
+        cout << obj.maxScore(cardPoints, N, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
