d54c503037fca55a40029ecbb188160a32ee044d
Rizwan Khan <113703223+Rizwankhan0001@users.noreply.github.com>
2023-11-01 14:57:04 +0530

    Time: 15 ms (69.51%), Space: 24.3 MB (82.92%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div501-find-mode-in-binary-search-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxTillNow = -1000000, curr = -1000000, prev = -1000000;
+    
+    void inOrder(TreeNode* root, vector<int> &ans) {        
+        if(root == NULL)
+            return;
+        inOrder(root->left, ans);
+		 
+		// the last visited element will be inOrder predecessor so if it occurs more than once, it has to be same here because it is a BST
+        if(root->val == prev)
+            curr++;
+        
+        else if(prev != root->val)
+            curr = 1;
+                
+        if(curr>maxTillNow) {
+            ans.clear();
+            ans.push_back(root->val);
+            maxTillNow = curr;
+        } 
+        else if(curr == maxTillNow)
+            ans.push_back(root->val);
+        
+        prev = root->val;
+        inOrder(root->right, ans);
+    }
+    
+    vector<int> findMode(TreeNode* root) {
+        vector<int> ans;
+        inOrder(root, ans);
+        return ans;
+    }
+};
